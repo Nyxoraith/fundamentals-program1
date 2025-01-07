93aa704e5918f717f593da6a121e98190f142f6e
Nyx <viniciuspimentelb@gmail.com>
2025-01-07 15:06:55 -0300

    Exercicio 1

---
+++ Aula 3 - Variaveis/exercicio1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+1) Elabore um programa que exiba o seu nome na primeira
+linha e o seu e-mail segunda. Em seguida, exiba uma
+mensagem solicitando para o usuário pressionar uma tecla.
+Quando o usuário pressionar, exiba em uma nova linha o nome
+do seu amigo e, em outra, o e-mail dele.
+*/
+
+int main(){
+    char meu_nome[] = "Vinicius";
+    char meu_email[] = "Vinicius@outlook.com";
+    char nome_amigo[] = "Leonardo";
+    char email_amigo[] = "Leonardo@yahoo.com";
+
+    printf("%s\n", meu_nome);
+    printf("%s\n", meu_email);
+
+    printf("Pressione uma tecla para continuar.");
+    getchar();
+    printf("%s\n", nome_amigo);
+    printf("%s\n", email_amigo);
+}
