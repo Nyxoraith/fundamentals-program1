e7a9ade7a025443f736f76346cb60fb8cb6dc0fe
Nyx <viniciuspimentelb@gmail.com>
2025-01-12 17:02:51 -0300

    Exercicios Lista 5-5

--- Lista P1: 01-09/exercicio5.c
+++ Lista P1: 01-09/exercicio5.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+5) Faça um programa que receba 2 números referentes ao numerador e denominador de uma fração e informe se é uma fração própria, imprópria, ou aparente
+*/
+
 int main(){
+    int numerador, denominador;
+
+    printf("Digite o numerador: ");
+    scanf("%d", &numerador);
+
+    printf("Digite o denominador: ");
+    scanf("%d", &denominador);
+
+    if (denominador == 0) {
+        printf("Erro: o denominador não pode ser zero.\n");
+        return 1;
+    }
+
+    if (numerador == 0) {
+        printf("A fração é aparente (fração resulta em 0).\n");
+    } else if (numerador % denominador == 0) {
+        printf("A fração é aparente (fração resulta em %d).\n", numerador / denominador);
+    } else if (numerador < denominador) {
+        printf("A fração é própria.\n");
+    } else {
+        printf("A fração é imprópria.\n");
+    }
 
     return 0;
 }
